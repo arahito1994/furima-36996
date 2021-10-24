1e0165df774ccc781d39afe3b1f513c0fe55aaeb
arahito1994 <artht0408@gmail.com>
2021-10-25 01:35:01 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,13 +14,22 @@ class Item < ApplicationRecord
   belongs_to :days_id
 
 
-
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
   validates :days_id, numericality: { other_than: 1 , message: "can't be blank" }
-
-
+  
+  with_options presence: true do
+    validates :image
+    validates :item_name
+    validates :item_text
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :area_id
+    validates :days_id
+    validates :price, format: { with: /\A[0-9]+\z/ }
+  end
 
 end
