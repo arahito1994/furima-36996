769b6030f9c8ebf1944a3d3fcaa8b607450f55f8
arahito1994 <artht0408@gmail.com>
2021-10-25 01:16:44 +0900

    Active Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,26 @@
 class Item < ApplicationRecord
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :postage_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :area_id
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :days_id
+
+
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :postage_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :area_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :days_id, numericality: { other_than: 1 , message: "can't be blank" }
+
+
+
 end
