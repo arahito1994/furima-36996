658d025e69cbfabb905d901e6d2dfdd0660d1061
arahito1994 <artht0408@gmail.com>
2021-10-26 11:58:09 +0900

    Itemモデルの単体テスト

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
     
-  end
+
+    item_name          { '商品名' }
+    item_text          { Faker::Lorem.sentence }
+    category_id        { 2 }
+    condition_id       { 2 }
+    postage_id         { 2 }
+    area_id            { 2 }
+    days_id            { 2 }
+    price              { 500 }
+    association :user
+
+    after(:build) do |items|
+      items.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+  end 
 end
