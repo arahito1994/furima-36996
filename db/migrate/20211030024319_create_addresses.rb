99e9d0f5f8bf5af8fd030700c0bb5a49605a19b8
arahito1994 <artht0408@gmail.com>
2021-10-30 15:25:19 +0900

    マイグレーションファイルの編集

--- db/migrate/20211030024319_create_addresses.rb
+++ db/migrate/20211030024319_create_addresses.rb
@@ -1,7 +1,13 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :post_number,      null: false
+      t.integer :area_id,         null: false
+      t.string :city,             null: false
+      t.string :street_number,    null: false
+      t.string :building
+      t.string :telephone,        null: false
+      t.references :record,       null: false, foreign_key: true
       t.timestamps
     end
   end
