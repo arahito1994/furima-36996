432f0fe13083d76f37b5d0bcff0f0574b88e74d9
arahito1994 <artht0408@gmail.com>
2021-10-30 12:53:52 +0900

    アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,11 @@ class Item < ApplicationRecord
   belongs_to :area
   belongs_to :days
 
+  has_one_attached :image
+  belongs_to :user
+  has_one :record
+
+
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :category_id
     validates :condition_id
@@ -26,6 +31,5 @@ class Item < ApplicationRecord
     validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is invalid' }
   end
 
-  has_one_attached :image
-  belongs_to :user
+  
 end
