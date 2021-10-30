93a030b6d602fc9b4240fb62e4d48b9f4e5bd8a8
arahito1994 <artht0408@gmail.com>
2021-10-30 11:48:33 +0900

    Formオブジェクト作成

---
+++ app/models/record_address.rb
@@ -0,0 +1,21 @@
+class RecordAddress
+  include ActiveModel::Model
+  attr_accessor, :user_id, :item_id, :post_number, :area_id, :city, :street_number, :building, :telephone
+
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
+    validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
+    validates :city
+    validates :street_number
+    validates :telephone, numericality: { only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11 }
+  end
+  
+  def save
+    record Record.create( user_id: user_id, item_id: item_id )
+    Adress.create( post_number: post_number, area_id: area_id, city: city, street_number: street_number, building: building, telephone: telephone, record_id: record.id )
+  end
+
+    
+end
