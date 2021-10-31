455261c8b01b03d36645a3b211a361618dbe83a2
arahito1994 <artht0408@gmail.com>
2021-10-31 16:26:23 +0900

    購入フォーム実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,2 +1,29 @@
 class RecordsController < ApplicationController
+
+  def index
+    @record_address = RecordAddress.new
+    @item = Item.new
+    @item = Item.find(params[:item_id])
+
+  end
+
+  def create
+    @record_address = RecordAddress.new(record_params)
+    @item = Item.new
+    @item = Item.find(params[:item_id])
+    if @record_address.valid?
+      @record_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record_address).permit(:post_number, :area_id, :city, :street_number, :building, :telephone).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+
 end
