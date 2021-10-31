34afe30614087d71ecdbe92d09140da618ae7e68
arahito1994 <artht0408@gmail.com>
2021-10-31 16:24:47 +0900

    バリデーション修正、エラー修正

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -1,6 +1,6 @@
 class RecordAddress
   include ActiveModel::Model
-  attr_accessor, :user_id, :item_id, :post_number, :area_id, :city, :street_number, :building, :telephone
+  attr_accessor :user_id, :item_id, :post_number, :area_id, :city, :street_number, :building, :telephone
 
   with_options presence: true do
     validates :user_id
@@ -9,12 +9,12 @@ class RecordAddress
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :street_number
-    validates :telephone, numericality: { only_integer: true, greater_than_or_equal_to: 10, less_than_or_equal_to: 11 }
+    validates :telephone, format: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 10, less_than_or_equal_to: 11, message: 'is invalid' }
   end
   
   def save
-    record Record.create( user_id: user_id, item_id: item_id )
-    Adress.create( post_number: post_number, area_id: area_id, city: city, street_number: street_number, building: building, telephone: telephone, record_id: record.id )
+    record = Record.create( user_id: user_id, item_id: item_id )
+    Address.create( post_number: post_number, area_id: area_id, city: city, street_number: street_number, building: building, telephone: telephone, record_id: record.id )
   end
 
     
