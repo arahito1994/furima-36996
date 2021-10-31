423312495b70deb6f5b4b56865a4491b36bf66ce
arahito1994 <artht0408@gmail.com>
2021-10-31 22:02:26 +0900

    RecordAddressモデルの単体テスト

---
+++ spec/factories/record_addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :record_address do
+    post_number             { '123-4567' }
+    area_id                 { 2 }
+    city                    { '横浜市緑区' }
+    street_number           { '青山1-1-1' }
+    building                { '柳ビル103' }
+    telephone               { '09012345678' }
+  end
+end
