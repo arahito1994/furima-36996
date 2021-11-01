5f89c9ae220145f0dacc64b31311cacca2ce200b
arahito1994 <artht0408@gmail.com>
2021-11-01 18:00:43 +0900

    バリデーションの追加

--- app/models/record_address.rb
+++ app/models/record_address.rb
@@ -5,6 +5,7 @@ class RecordAddress
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_number, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
